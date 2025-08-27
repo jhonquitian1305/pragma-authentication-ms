9991c3daea32e54944474e22538d26d6ce0d0b37
Jhon Quitian <quitian.j2010@gmail.com>
2025-08-27 15:18:01 -0500

    feat: create model user and userRepository

---
+++ domain/model/src/main/java/co/com/pragma/model/user/User.java
@@ -0,0 +1,22 @@
+package co.com.pragma.model.user;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@ToString
+public class User {
+    private Long id;
+    private String name;
+    private String lastname;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String email;
+    private Double baseSalary;
+    private Boolean isActive;
+}
---
+++ domain/model/src/main/java/co/com/pragma/model/user/gateways/UserRepository.java
@@ -0,0 +1,9 @@
+package co.com.pragma.model.user.gateways;
+
+import co.com.pragma.model.user.User;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository {
+    Mono<User> saveOne(User user);
+    Mono<Boolean> isUniqueEmail(String email);
+}
