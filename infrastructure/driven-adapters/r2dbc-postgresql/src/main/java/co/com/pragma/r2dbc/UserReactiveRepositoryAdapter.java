32ac2c7af19bb3431b128820cce1e2e533aa2b26
Jhon Quitian <quitian.j2010@gmail.com>
2025-08-27 16:32:34 -0500

    feat: add logs in repository adapter

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserReactiveRepositoryAdapter.java
@@ -4,11 +4,13 @@ import co.com.pragma.model.user.User;
 import co.com.pragma.model.user.gateways.UserRepository;
 import co.com.pragma.r2dbc.entity.UserEntity;
 import co.com.pragma.r2dbc.helper.ReactiveAdapterOperations;
+import lombok.extern.slf4j.Slf4j;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
+@Slf4j
 public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
     User,
     UserEntity,
@@ -21,11 +23,13 @@ public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
 
     @Override
     public Mono<User> saveOne(User user) {
+        log.info("saving a user");
         return super.save(user);
     }
 
     @Override
     public Mono<Boolean> isUniqueEmail(String email) {
+        log.info("searching if email is unique");
         return this.repository.existsByEmail(email);
     }
 }
