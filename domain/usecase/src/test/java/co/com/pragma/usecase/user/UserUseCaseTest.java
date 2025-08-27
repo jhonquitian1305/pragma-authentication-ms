f8a2f4e051e44215f51baa4015efdd2ef7117227
Jhon Quitian <quitian.j2010@gmail.com>
2025-08-27 17:14:08 -0500

    feat: create test to UserUseCase

---
+++ domain/usecase/src/test/java/co/com/pragma/usecase/user/UserUseCaseTest.java
@@ -0,0 +1,70 @@
+package co.com.pragma.usecase.user;
+
+import co.com.pragma.model.user.User;
+import co.com.pragma.model.user.gateways.UserRepository;
+import co.com.pragma.usecase.user.exception.EmailExistsException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.*;
+
+class UserUseCaseTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserUseCase userUseCase;
+
+    private User user;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        user = User.builder()
+                .name("John")
+                .lastname("Doe")
+                .birthDate(LocalDate.parse("2025-12-25"))
+                .address("en casa")
+                .phone("3254887894")
+                .email("correo@mail.com")
+                .baseSalary(1200.0)
+                .build();
+    }
+
+    @Test
+    void shouldSaveOneUserWhenEmailIsUnique(){
+        when(this.userRepository.isUniqueEmail(user.getEmail())).thenReturn(Mono.just(false));
+        when(this.userRepository.saveOne(user)).thenReturn(Mono.just(user));
+
+        StepVerifier.create(this.userUseCase.saveOne(user))
+                .expectNext(user)
+                .verifyComplete();
+
+        verify(this.userRepository).isUniqueEmail(user.getEmail());
+        verify(this.userRepository).saveOne(user);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailExists(){
+        when(this.userRepository.isUniqueEmail(user.getEmail())).thenReturn(Mono.just(true));
+
+        StepVerifier.create(this.userUseCase.saveOne(user))
+                .expectErrorSatisfies(ex -> {
+                    assert ex instanceof EmailExistsException;
+                    assert ex.getMessage().equals("El email debe ser único");
+                })
+                .verify();
+
+        verify(this.userRepository).isUniqueEmail(user.getEmail());
+        verify(this.userRepository, never()).saveOne(any());
+    }
+
+
+}
