f4428ec5ded45eb38589593486dd59335697f751
Jhon Quitian <quitian.j2010@gmail.com>
2025-09-01 18:10:45 -0500

    feat: add environment variables and schema.sql

---
+++ applications/app-service/src/main/resources/db/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   lastname VARCHAR(100),
+   birth_date DATE,
+   address VARCHAR(255),
+   phone VARCHAR(20),
+   dni VARCHAR(150) UNIQUE NOT NULL,
+   email VARCHAR(150) UNIQUE NOT NULL,
+   base_salary NUMERIC(10,2),
+   is_active BOOLEAN DEFAULT TRUE
+);
